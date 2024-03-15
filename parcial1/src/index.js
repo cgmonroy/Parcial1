bbf21c78aafe3ff953d21a5be475ac3a3a9d4b93
Carlos Monroy <cg.monroy@uniandes.edu.co>
2024-03-15 09:19:20 -0500

    parcial parte 1

--- parcial1/src/index.js
+++ parcial1/src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import FormularioAut from "./components/FormularioAut"
+import Detalle from "./components/Detalle"
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<FormularioAut />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
